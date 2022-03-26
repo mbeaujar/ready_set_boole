4bad3f1cb571515955576cfd07a7b5c0f4b33e4c
mbeaujar <mbeaujar@student.42.fr>
2022-03-26 23:32:06 +0100

    fix(ex00): optimize Adder

--- 00-Adder/main.cpp
+++ 00-Adder/main.cpp
@@ -3,7 +3,7 @@
 
 // https://www.geeksforgeeks.org/bitwise-operators-in-c-cpp/
 
-int adder(int num1, int num2) {
+int adderV1(int num1, int num2) {
   int b1, b2, sum = 0, ret = 0, bit;
 
   if (num1 == 0 && num2 == 0) return 0;
@@ -31,15 +31,40 @@ int adder(int num1, int num2) {
       }
     }
     sum = bit ? sum ^ (1 << i) : sum;
-    // o(1) ??
-    // if (num1 >> i == 0 && num2 >> i == 0 && ret == 0) break;
+  }
+  return sum;
+}
+
+// Inputs |	Outputs
+//  A	 B	|  C	S
+//  0	 0	|  0	0
+//  0	 1	|  0	1
+//  1	 0	|  0	1
+//  1	 1	|  1	0
+
+int adder(int num1, int num2) {
+  int sum = 0;
+  bool a, b, carry = 0;
+
+  for (int i = 0; i < 32; i++) {
+    a = (1 << i) & num1;
+    b = (1 << i) & num2;
+
+    sum = (a ^ b ^ carry) ? sum ^ (1 << i) : sum;
+
+    // case where we have 1 ^ 1 = 0
+    carry = ((a & b) | (a & carry) | (b & carry));
+
+    // not O(1) anymore
+    // if (num1 >> i == 0 && num2 >> i == 0 && carry == 0) break;
   }
   return sum;
 }
 
 int main() {
-  int num1 = 2345;
-  int num2 = 8124;
-  std::cout << "total sum: " << adder(num1, num2) << std::endl;
+  int num1 = 80, num2 = 1000;
+
+  std::cout << adder(num1, num2);
+
   return 0;
 }
