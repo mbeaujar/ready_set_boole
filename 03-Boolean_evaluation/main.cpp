131924c2c4ccbc6f71a42b8f05a2fc32b509b6a1
mbeaujar <mbeaujar@student.42.fr>
2022-03-27 16:57:11 +0200

    feat(ex03): add eval_formula without AST

---
+++ 03-Boolean_evaluation/main.cpp
@@ -0,0 +1,76 @@
+#include <iomanip>
+#include <iostream>
+#include <stack>
+
+// #include "ASTree.hpp"
+
+bool isBinOp(char c) {
+  return c == '&' || c == '|' || c == '>' || c == '=' || c == '^' || c == '!';
+}
+
+bool calcul(char ope, bool num1, bool num2) {
+  switch (ope) {
+    case '|':
+      return num1 | num2;
+    case '&':
+      return num1 & num2;
+    case '>':
+      return !(num1 && !num2);
+    case '=':
+      return num1 == num2;
+    case '^':
+      return num1 ^ num2;
+    case '!':
+      return num1 == !num2;
+    default:
+      std::cerr << "Error: wrong char: " << ope << std::endl;
+      break;
+  }
+  return 0;
+}
+
+bool eval_formula(std::string& str) {
+  std::stack<bool> values;
+  std::stack<char> op;
+
+  for (size_t i = 0; i < str.length(); i++) {
+    if (str[i] == ' ') continue;
+    if (str[i] == '0' || str[i] == '1') {
+      values.push(str[i] - 48);
+    } else if (isBinOp(str[i])) {
+      op.push(str[i]);
+    } else {
+      std::cerr << "Invalid character: " << str[i] << std::endl;
+      exit(1);
+    }
+  }
+
+  while (!op.empty()) {
+    if (values.size() < 2) {
+      std::cerr << "Wrong number of operators and operands" << std::endl;
+      exit(1);
+    }
+    char ope = op.top();
+    op.pop();
+
+    bool num1 = values.top();
+    values.pop();
+
+    bool num2 = values.top();
+    values.pop();
+
+    values.push(calcul(ope, num1, num2));
+  }
+  if (values.size() == 0) return 0;
+  return values.top();
+}
+
+int main(int ac, char** av) {
+  if (ac != 2) {
+    std::cerr << "Wrong number of arguments" << std::endl;
+    return 1;
+  }
+  std::string expression = av[1];
+  std::cout << std::boolalpha << eval_formula(expression) << std::endl;
+  return 0;
+}
